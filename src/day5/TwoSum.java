34722eb7a94bd4a43cac07cedd6a752458ac227c
MeghaNaut <meghanautiyal20@gmail.com>
2026-03-17 13:08:00 +0530

    two sum done!!

--- src/day5/TwoSum.java
+++ src/day5/TwoSum.java
@@ -1,19 +1,41 @@
 package day5;
 
-public class TwoSum {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr= {2,4,5,1,3};
-		int target=5;
-		for(int i=0;i<arr.length-1;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[i]+arr[j]==target) {
-					System.out.println(i +" , "+j);
+
+public class TwoSum {
+	
+	public static void findTarget(int[] a,int t) {
+		for(int i=0;i<a.length-1;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]+a[j]==t) {
+					
+					System.out.println("["+i+" , "+j+"]");
+					return;
 				}
 			}
+			
 		}
+		System.out.println("no pair found!!");
+		
+	}
 
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter array length");
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		System.out.println("Enter array elements");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();		
+		}
+		
+		System.out.println("enter target");
+		int target=sc.nextInt();
+		sc.close();
+		findTarget(arr,target);
+		
 	}
 
 }
