85c7329a967f7383dfc7365476090fc3ace18d74
MeghaNaut <meghanautiyal20@gmail.com>
2026-03-17 15:34:14 +0530

    Array pallindrome done

---
+++ src/day6/ArrayPalindrome.java
@@ -0,0 +1,32 @@
+package day6;
+
+public class ArrayPalindrome {
+
+	public static boolean isPalindrome(int[] arr) {
+		
+		
+		int left=0;
+		int right=arr.length-1;
+		while(left<right) {
+			if(arr[left]!=arr[right]) {
+				return false;
+			}
+			left++;right--;
+		}
+
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {};
+		if(arr.length==0) {
+			System.out.println("Array is empty!!");
+			return;
+		}
+		System.out.println(isPalindrome(arr));
+		
+		
+	}
+
+}
